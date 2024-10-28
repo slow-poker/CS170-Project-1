ec01cb4e013336e92360191efb663232a7ce2ecb
slow-poker <nolan@DESKTOP-UC9N8I3>
2024-10-28 09:53:30 -0700

    fixed typo

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main() {
 
 //problem space in tree, 4 children per (child1, child2, child3), include puzzle (.puzzle), h(n) (heurisitc), f(n) (node), .visited (boolean)
 //store puzzle in 3D array   (puzzle) 
-// store fronteir in priorty queue (frontier)
+// store frontier in priorty queue (frontier)
 
 
 //puzzle and swapping, euclidian distance
